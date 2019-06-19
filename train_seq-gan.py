4c2084878f278ca0b46bab74f14850060b29513d
mutnauq <clement.tabary@polytechnique.edu>
2019-06-19 20:07:45 +0100

    change train process (need colab adapt)

--- train_seq-gan.py
+++ train_seq-gan.py
@@ -16,7 +16,8 @@ from visualize import frame_to_vid
 GPU = False
 device_idx = 0
 if GPU:
-    device = torch.device("cuda:" + str(device_idx) if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:" + str(device_idx)
+                          if torch.cuda.is_available() else "cpu")
 else:
     device = torch.device("cpu")
 print(device)
@@ -25,11 +26,11 @@ if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
 torch.manual_seed(0)
 
-if not os.path.exists('./samples'):
-    os.makedirs('./samples')
+if not os.path.exists('./runs'):
+    os.makedirs('./runs')
 
-if not os.path.exists('./models'):
-    os.makedirs('./models')
+logdir = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '_seq-wgan-gp'
+os.makedirs('./runs/' + logdir)
 
 # Load dataset
 datasetf = 'Music-to-Dance-Motion-Synthesis-master'
@@ -50,16 +51,16 @@ lr = 0.0002
 n_critic_steps = 8
 
 # Loading dataset
-logdir = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '_seq-wgan-gp'
-writer = SummaryWriter('./runs/' + logdir)
-# tbc = tensorboardcolab.TensorBoardColab(graph_path='./runs/'+logdir)  # for Colab
-os.makedirs('./samples/' + logdir)
+writer = SummaryWriter('./runs/' + logdir + '/logging')
+sampledir = './runs/' + logdir + '/samples'
+os.makedirs(sampledir)
 dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn,
                         sampler=SubsetRandomSampler(range(num_train)))
 n_iter = len(dataloader)
 
 # Creating models
-fixed_noise = torch.randn(nb_samples, seq_length, latent_vector_size, device=device)
+fixed_noise = torch.randn(nb_samples, seq_length,
+                          latent_vector_size, device=device)
 
 gen = Generator(latent_vector_size, size, output_size, nblocks)
 sum(p.numel() for p in gen.parameters() if p.requires_grad)
@@ -72,73 +73,68 @@ critic.to(device)
 optim_critic = optim.Adam(critic.parameters(), lr=lr)
 optim_gen = optim.Adam(gen.parameters(), lr=lr)
 
-losses_critic = []
-losses_gen = []
+total_iterations = 0
 
 # Training process : "Sequential" Wasserstein GAN-GP
 for epoch in range(num_epochs):
     gen.train()
     for idx, (real, _, _, _) in enumerate(dataloader):
+        total_iterations += 1
+
+        # Train discriminator critic n_critic_steps times
+        optim_critic.zero_grad()
+
+        noise = torch.randn(batch_size, seq_length,
+                            latent_vector_size, device=device)
+        fake = gen(noise, [seq_length] * batch_size)
+        fake = fake.view(batch_size, seq_length, output_size).permute(
+            0, 2, 1).contiguous()
         real = real.to(device)
-        loss_critic = 0
-        for _ in range(n_critic_steps):
-            # Train discriminator critic n_critic_steps times
-            optim_critic.zero_grad()
-            # TODO: train with random-length sequences of noise
-            noise = torch.randn(batch_size, seq_length, latent_vector_size, device=device)
-            fake = gen(noise, [seq_length] * batch_size)
-            fake = fake.view(batch_size, seq_length, output_size).permute(0, 2, 1).contiguous()
-            real_s = extract_at_random(real, seq_length)
-            real_s = real_s.view(batch_size, seq_length, output_size).permute(0, 2, 1).contiguous()
-            gp = gradient_penalty(critic, batch_size, real_s, fake, is_seq=True, device=device)
-            err_real = torch.mean(critic(real_s))
-            err_fake = torch.mean(critic(fake.detach()))
-            err_critic = err_fake - err_real + gamma * gp
-            loss_critic += err_critic.item()
-            err_critic.backward(retain_graph=True)
-            optim_critic.step()
-        loss_critic /= n_critic_steps
+        real_s = extract_at_random(real, seq_length)
+        real_s = real_s.view(batch_size, seq_length,
+                             output_size).permute(0, 2, 1).contiguous()
+
+        gp = gradient_penalty(critic, batch_size,
+                              real_s, fake, is_seq=True, device=device)
+
+        # Compute wasserstein distance
+        err_real = torch.mean(critic(real_s))
+        err_fake = torch.mean(critic(fake.detach()))
+        err_critic = err_fake - err_real + gamma * gp
+        loss_critic = err_critic.item()
+        err_critic.backward(retain_graph=True)
+        optim_critic.step()
+
+        if total_iterations % n_critic_steps:
+            continue
 
         # Train generator gen
-        loss_gen = 0
         optim_gen.zero_grad()
-        # TODO: train with random-length sequences of noise
-        noise = torch.randn(batch_size, seq_length, latent_vector_size, device=device)
+        noise = torch.randn(batch_size, seq_length,
+                            latent_vector_size, device=device)
         fake = gen(noise, [seq_length] * batch_size)
-        fake = fake.view(batch_size, seq_length, output_size).permute(0, 2, 1)
+        fake = fake.view(batch_size, seq_length, output_size).permute(
+            0, 2, 1).contiguous()
         err_real = torch.mean(critic(real_s))
         err_fake = torch.mean(critic(fake))
         err_gen = err_real - err_fake
-        loss_gen += err_gen.item()
+        loss_gen = err_gen.item()
         err_gen.backward()
         optim_gen.step()
 
-        # writer.add_scalar('loss_critic', loss_critic, idx + epoch * n_iter)
-        # writer.add_scalar('loss_gen', loss_gen, idx + epoch * n_iter)
+        # writer.add_scalar('loss_critic', loss_critic, total_iterations)
+        # writer.add_scalar('loss_gen', loss_gen, total_iterations)
 
-        # for name, param in gen.named_parameters():
-        #     writer.add_histogram(name, param.data.numpy(), idx + epoch * n_iter)
-        # for name, param in gen.named_parameters():
-        #     writer.add_histogram(name, param.data.numpy(), idx + epoch * n_iter)
-
-        # tbc.save_value('Loss D', 'lossD', idx + epoch * n_iter, loss_critic)  # Colab
-        # tbc.save_value('Loss G', 'lossG', idx + epoch * n_iter, loss_gen)
-
-    losses_critic.append(loss_critic)
-    losses_gen.append(loss_gen)
-    print('Epoch {}/{} : loss_critic: {} loss_gen: {}'.format(epoch+1, num_epochs,
-                                                              loss_critic, loss_gen))
-
-    if (epoch+1) % 100 == 0:
+    if (epoch + 1) % 100 == 0:
         gen.eval()
         print("Generating sample animations...")
         samples = sampleseqG(gen, seq_length, fixed_noise)
-        samples = np.reshape(samples, (seq_length*nb_samples, -1))
+        samples = np.reshape(samples, (seq_length * nb_samples, -1))
         if dataset.scaler is not None:
             samples = dataset.scaler.inverse_transform(samples)
         samples = np.reshape(samples, (nb_samples, seq_length, 23, 3))
         for s in range(nb_samples):
-            filepath = './samples/' + logdir + '/e{}s{}.avi'.format(epoch+1, s+1)
+            filepath = sampledir + '/e{}s{}.avi'.format(epoch + 1, s + 1)
             frame_to_vid(samples[s, :], filepath, fps=25)
 
 # torch.save(gen.state_dict(), './models/' + logdir + '_gen_{}.pt'.format(num_epochs))
