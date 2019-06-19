98cd6008e7788c9b62b6f3d82a00efc515c944bc
mutnauq <clement.tabary@polytechnique.edu>
2019-06-19 20:03:29 +0100

    beautify

--- visualize.py
+++ visualize.py
@@ -57,22 +57,22 @@ def to_2d_graph_data(frame):
         node_trace['y'] += tuple([y])
 
     # Add special nodes and edges
-    x0, y0 = (G.node[0]['pos'] + G.node[1]['pos'])/2
-    x1, y1 = (G.node[3]['pos'] + G.node[12]['pos'])/2
+    x0, y0 = (G.node[0]['pos'] + G.node[1]['pos']) / 2
+    x1, y1 = (G.node[3]['pos'] + G.node[12]['pos']) / 2
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
 
-    x0, y0 = (G.node[3]['pos'] + G.node[12]['pos'])/2
+    x0, y0 = (G.node[3]['pos'] + G.node[12]['pos']) / 2
     x1, y1 = G.node[2]['pos']
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
 
-    x0, y0 = (G.node[10]['pos'] + G.node[11]['pos'])/2
+    x0, y0 = (G.node[10]['pos'] + G.node[11]['pos']) / 2
     x1, y1 = G.node[9]['pos']
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
 
-    x0, y0 = (G.node[19]['pos'] + G.node[20]['pos'])/2
+    x0, y0 = (G.node[19]['pos'] + G.node[20]['pos']) / 2
     x1, y1 = G.node[18]['pos']
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
@@ -133,25 +133,25 @@ def to_3d_graph_data(frame):
         node_trace['z'] += tuple([z])
 
     # Add special nodes and edges
-    x0, y0, z0 = (G.node[0]['pos'] + G.node[1]['pos'])/2
-    x1, y1, z1 = (G.node[3]['pos'] + G.node[12]['pos'])/2
+    x0, y0, z0 = (G.node[0]['pos'] + G.node[1]['pos']) / 2
+    x1, y1, z1 = (G.node[3]['pos'] + G.node[12]['pos']) / 2
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
     edge_trace['z'] += tuple([z0, z1, None])
 
-    x0, y0, z0 = (G.node[3]['pos'] + G.node[12]['pos'])/2
+    x0, y0, z0 = (G.node[3]['pos'] + G.node[12]['pos']) / 2
     x1, y1, z1 = G.node[2]['pos']
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
     edge_trace['z'] += tuple([z0, z1, None])
 
-    x0, y0, z0 = (G.node[10]['pos'] + G.node[11]['pos'])/2
+    x0, y0, z0 = (G.node[10]['pos'] + G.node[11]['pos']) / 2
     x1, y1, z1 = G.node[9]['pos']
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
     edge_trace['z'] += tuple([z0, z1, None])
 
-    x0, y0, z0 = (G.node[19]['pos'] + G.node[20]['pos'])/2
+    x0, y0, z0 = (G.node[19]['pos'] + G.node[20]['pos']) / 2
     x1, y1, z1 = G.node[18]['pos']
     edge_trace['x'] += tuple([x0, x1, None])
     edge_trace['y'] += tuple([y0, y1, None])
@@ -208,27 +208,34 @@ def draw(frame, size):
                  (int(frame[f][0]), int(frame[f][1])),
                  color, 2)
     cv2.line(cvs,
-             (int((frame[0][0]+frame[1][0])/2), int((frame[0][1]+frame[1][1])/2)),
-             (int((frame[3][0]+frame[12][0])/2), int((frame[3][1]+frame[12][1])/2)),
+             (int((frame[0][0] + frame[1][0]) / 2),
+              int((frame[0][1] + frame[1][1]) / 2)),
+             (int((frame[3][0] + frame[12][0]) / 2),
+              int((frame[3][1] + frame[12][1]) / 2)),
              color, 2)
     cv2.line(cvs,
              (int(frame[3][0]), int(frame[3][1])),
-             (int((frame[3][0]+frame[12][0])/2), int((frame[3][1]+frame[12][1])/2)),
+             (int((frame[3][0] + frame[12][0]) / 2),
+              int((frame[3][1] + frame[12][1]) / 2)),
              color, 2)
     cv2.line(cvs,
              (int(frame[12][0]), int(frame[12][1])),
-             (int((frame[3][0]+frame[12][0])/2), int((frame[3][1]+frame[12][1])/2)),
+             (int((frame[3][0] + frame[12][0]) / 2),
+              int((frame[3][1] + frame[12][1]) / 2)),
              color, 2)
     cv2.line(cvs, (int(frame[2][0]), int(frame[2][1])),
-             (int((frame[3][0]+frame[12][0])/2), int((frame[3][1]+frame[12][1])/2)),
+             (int((frame[3][0] + frame[12][0]) / 2),
+              int((frame[3][1] + frame[12][1]) / 2)),
              color, 2)
     cv2.line(cvs,
              (int(frame[9][0]), int(frame[9][1])),
-             (int((frame[10][0]+frame[11][0])/2), int((frame[10][1]+frame[11][1])/2)),
+             (int((frame[10][0] + frame[11][0]) / 2),
+              int((frame[10][1] + frame[11][1]) / 2)),
              color, 2)
     cv2.line(cvs,
              (int(frame[18][0]), int(frame[18][1])),
-             (int((frame[19][0]+frame[20][0])/2), int((frame[19][1]+frame[20][1])/2)),
+             (int((frame[19][0] + frame[20][0]) / 2),
+              int((frame[19][1] + frame[20][1]) / 2)),
              color, 2)
     frame[:, 0] -= size[0] // 2
     frame[:, 1] -= size[1] // 2
@@ -238,7 +245,8 @@ def draw(frame, size):
 def frame_to_vid(frames, name, fps):
     w, h = 300, 300
     size = (w, h, 3)
-    video = cv2.VideoWriter(name, cv2.VideoWriter_fourcc(*"MJPG"), float(fps), (h, w))
+    video = cv2.VideoWriter(
+        name, cv2.VideoWriter_fourcc(*"MJPG"), float(fps), (h, w))
     for _ in range(len(frames)):
         video.write(draw(frames[_], size).astype('uint8'))
     video.release()
@@ -269,4 +277,4 @@ if __name__ == '__main__':
     # Save 2D animation
     seq, _, dir = seq_dataset[0]
     # librosa.output.write_wav(dir+'/audio_extract.wav', np.asarray(audio), sr=44100)
-    frame_to_vid(seq, dir+'/animation_extract.avi', fps=5)
+    frame_to_vid(seq, dir + '/animation_extract.avi', fps=25)
